599114b5af2bfdecdffc66d309254c3729495094
P-Serg <163842794+P-Serg@users.noreply.github.com>
2024-04-14 00:08:23 +0500

    add save file

--- main.py
+++ main.py
@@ -1,40 +1,44 @@
-class Animal: # создание базового класса животные
-    def __init__(self, name, age): # создание атрибутов базового класса
-        self.name = name
-        self.age = age
+# hw-th4-lessonOB3-zoo
+
+import pickle  # импорт модуля pickle для сохранения данных в файл
+
+class Animal(): # создание базового класса животного
+    def __init__(self, name, age): # создание атрибутов базового класса животного
+        self.name = name # создание специфичного атрибута имя животного
+        self.age = age # создание специфичного атрибута возраст животного
 
     def make_sound(self): # создание метода голоса животного
         pass
 
-    def eat(self): # создание метода животное ест
+    def eat(self):  # создание метода животное ест
         pass
 
 
-class Bird(Animal):  # создание подкласса Bird
-    def __init__(self, name, age, wingspan):  # создание атрибутов подкласса
+class Bird(Animal):  # создание подкласса птицы
+    def __init__(self, name, age, wingspan):  # создание атрибутов подкласса птицы
         super().__init__(name, age)  # вызов метода родительского класса
-        self.wingspan = wingspan  # создание специфичного атрибута подкласса
+        self.wingspan = wingspan  # создание специфичного атрибута птицы
 
     def make_sound(self):  # создание метода голоса птицы
         return "Карамба"
 
 
-class Mammal(Animal):  # создание подкласса млекопитающие
-    def __init__(self, name, age, fur_color):  # создание атрибутов подкласса
+class Mammal(Animal):  # создание подкласса млекопитающих
+    def __init__(self, name, age, fur_color):  # создание атрибутов подкласса млекопитающих
         super().__init__(name, age)  # вызов метода родительского класса
-        self.fur_color = fur_color  # создание специфичного атрибута подкласса
+        self.fur_color = fur_color  # создание специфичного атрибута млекопитающих
 
-    def make_sound(self): # создание метода голоса млекопитающего
-        return "Рррррр"
+    def make_sound(self):  # создание метода голоса млекопитающих
+        return "Рррр"
 
 
-class Reptile(Animal): # создание подкласса рептилии
-    def __init__(self, name, age, scale_color): # создание атрибутов подкласса рептилии
+class Reptile(Animal):  # создание подкласса рептилий
+    def __init__(self, name, age, scale_color):  # создание атрибутов подкласса рептилий
         super().__init__(name, age)  # вызов метода родительского класса
-        self.scale_color = scale_color  # создание специфичного атрибута подкласса
+        self.scale_color = scale_color  # создание специфичного атрибута рептилий
 
-    def make_sound(self): # создание метода голоса рептилии
-        return "Сссссс"
+    def make_sound(self):  # создание метода голоса рептилий
+        return "Сссс"
 
 
 def animal_sound(animals):  # функция голоса животных
@@ -43,54 +47,67 @@ def animal_sound(animals):  # функция голоса животных
 
 
 class Zoo:  # создание класса зоопарк
-    def __init__(self): # создание атрибутов
-        self.animals = [] # создание списка животных
-        self.staff = [] # создание списка сотрудников
+    def __init__(self):  # создание атрибутов
+        self.animals = []  # создание списка животных
+        self.staff = []  # создание списка персонала
 
-    def add_animal(self, animal): # добавление животных в зоопарк
-        self.animals.append(animal) # добавление животного в список животных
+    def add_animal(self, animal):   # добавление животных в зоопарк
+        self.animals.append(animal)
 
-    def add_staff(self, staff): # добавление сотрудников в класс зоопарк
-        self.staff.append(staff) # добавление сотрудника в список сотрудников
+    def add_staff(self, staff):  # добавление сотрудников в класс зоопарк
+        self.staff.append(staff)
 
+    def save_zoo(self, filename):  # сохранение данных в файл
+        with open(filename, 'wb') as file: # запись данных в файл
+            pickle.dump(self, file)
 
-class ZooKeeper: # создание класса смотритель зоопарка
-    def __init__(self, name): # создание атрибутов сотрудника смотритель зоопарка
-        self.name = name # создание специфичного атрибута имя сотрудника
-
-    def feed_animal(self, animal): # добавление животного в список животных сотрудника смотрителя зоопарка
-        print(f"{self.name} кормит {animal.name}") # вывод метода голоса животного
-
-
-class Veterinarian: # создание класса ветеринар
-    def __init__(self, name):    # создание атрибутов сотрудника ветеринар
-        self.name = name # создание специфичного атрибута имя сотрудника ветеринара
-
-    def heal_animal(self, animal): # добавление животного в список животных сотрудника ветеринара
-        print(f"{self.name} лечит {animal.name}")  # вывод метода голоса животного
+    def load_zoo(filename):  # загрузка данных из файл
+        with open(filename, 'rb') as file: # чтение данных из файл
+            return pickle.load(file)
 
 
+class ZooKeeper:  # создание подкласса сотрудников смотритель зоопарка
+    def __init__(self, name):
+        self.name = name
+
+    def feed_animal(self, animal): #  кормление животного
+        print(f"{self.name} кормит {animal.name}")
 
 
-bird = Bird("Попугай", 5, "зеленые")
-mammal = Mammal("Лев", 3, "Желтый")
-reptile = Reptile("Змея", 2, "Коричневая")
+class Veterinarian:  # создание подкласса сотрудников ветеринар зоопарка
+    def __init__(self, name):
+        self.name = name
 
-zookeeper = ZooKeeper("Иван")
-veterinarian = Veterinarian("Семен Петрович")
+    def heal_animal(self, animal): # лечение животного
+        print(f"{self.name} лечит {animal.name}")
 
+
+# Создаем зоопарк и добавляем животных и персонал
 zoo = Zoo()
+bird = Bird("Попугай", 5, "зеленый")
+mammal = Mammal("Лев", 3, "желтый")
+reptile = Reptile("Змея", 2, "коричневый")
+bird1 = Bird("Павлин", 4, "синий")
 zoo.add_animal(bird)
 zoo.add_animal(mammal)
 zoo.add_animal(reptile)
+zoo.add_animal(bird1)
 
+zookeeper = ZooKeeper("Иван")
+veterinarian = Veterinarian("Семен Петрович")
 zoo.add_staff(zookeeper)
 zoo.add_staff(veterinarian)
 
-for staff_member in zoo.staff:
-    if isinstance(staff_member, ZooKeeper):
-        staff_member.feed_animal(bird)
-    elif isinstance(staff_member, Veterinarian):
-        staff_member.heal_animal(mammal)
+# Сохраняем зоопарк в файл
+zoo.save_zoo("zoo.pkl")
 
-animal_sound([bird, mammal, reptile])
+# Загружаем зоопарк из файла
+loaded_zoo = Zoo.load_zoo("zoo.pkl")
+
+# Выводим информацию о загруженном зоопарке
+print("Животные в зоопарке:")
+for animal in loaded_zoo.animals:
+    print(f"{animal.name} - {type(animal).__name__}")
+print("Сотрудники зоопарка:")
+for staff in loaded_zoo.staff:
+    print(staff.name)
